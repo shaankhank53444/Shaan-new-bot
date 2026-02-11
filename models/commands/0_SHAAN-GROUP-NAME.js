8590475db915914b57b13f3bb07aa6684667ff4f
Shaanabot <shaankhank325@gmail.com>
2026-02-11 23:25:09 +0500

    Update and rename 0_ARIF-GROUP-NAME.js to 0_SHAAN-GROUP-NAME.js

--- models/commands/0_SHAAN-GROUP-NAME.js
+++ models/commands/0_SHAAN-GROUP-NAME.js
@@ -2,8 +2,8 @@ module.exports.config = {
 	name: "groupname",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "ARIF BABU",
-	description: "THIS BOT WAS MADE BY MR ARIF BABU",
+	credits: "SHAAN BABU",
+	description: "THIS BOT WAS MADE BY MR SHAAN BABU",
 	commandCategory: "CHANGE GROUP NAME", 
 	usages: "PREFIX", 
 	cooldowns: 0,
@@ -13,5 +13,5 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
 	var name = args.join(" ")
 	if (!name) api.sendMessage("BOSS SAATH ME GROUP KANAME LIKHO JO AAP RAKHNA CHAHTA HO 😐✌️", event.threadID, event.messageID)
-	else api.setTitle(name, event.threadID, () => api.sendMessage(`AB IS GROUP KA NAME YAHI HA 👉 ${name}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR ARIF BABU 🌺`, event.threadID, event.messageID));
+	else api.setTitle(name, event.threadID, () => api.sendMessage(`AB IS GROUP KA NAME YAHI HA 👉 ${name}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR SHAAN BABU 🌺`, event.threadID, event.messageID));
 }
